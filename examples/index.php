f6c0a41a1c36a1b89546343e6d204c1833e03b4e
Nike <nicolasmaquet@gmail.com>
2017-09-10 21:32:55 +0200

    mets à jour le fichier example et compresse les images

--- examples/index.php
+++ examples/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $pathLinkFile="./assets/";
 ?>
 
@@ -6,13 +6,13 @@ $pathLinkFile="./assets/";
 <html lang="fr">
 <head>
 	<title>formflex</title>
-	
+
 	<meta name="description" content="Description courte de la page" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta http-equiv="Content-Language" content="fr" />
 	<meta http-equiv="Content-Script-Type" content="text/javascript" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 	<link href="<?php echo $pathLinkFile; ?>css/style.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 	<link href="<?php echo $pathLinkFile; ?>img/favicon.png" rel="shortcut icon" type="image/png" />
@@ -24,7 +24,7 @@ $pathLinkFile="./assets/";
 	<i class="fa fa-link"></i> SliKer
 </h1>
 
-<div id="slider_classique" class="sliker safeload vam" data-arrow="fa fa-caret" data-bullet="fa fa-star">
+<div id="slider_classique" class="sliker safeload" data-arrow="fa fa-caret" data-bullet="fa fa-star">
 	<div class="conteneur_strict">
 		<ul class="grand_slider">
 			<?php for($i=1;$i<=10;$i++){ ?>
@@ -39,6 +39,47 @@ $pathLinkFile="./assets/";
 		</ul>
 	</div>
 </div>
+
+<blockquote>
+&lt;div id="slider_classique" class="sliker safeload" data-arrow="fa fa-caret" data-bullet="fa fa-star">
+	&lt;div class="conteneur_strict">
+		&lt;ul class="grand_slider">
+			&lt;?php for($i=1;$i&lt;=10;$i++){ ?>
+			&lt;li class="&lt;?php if($i==3){echo "selected";} ?>">
+				&lt;div class="block-3-2">
+					&lt;span>
+						&lt;img src="&lt;?php echo $pathLinkFile; ?>img/examples/&lt;?php echo $i; ?>.jpg">
+					&lt;/span>
+				&lt;/div>
+			&lt;/li>
+			&lt;?php } ?>
+		&lt;/ul>
+	&lt;/div>
+&lt;/div>
+
+$('#slider_classique').sliker({
+'nbr_li':1, //nombre d'éléments qui défilent à chaque mouvement.
+'vitesse_auto':3000, //temps entre deux mouvements automatiques.
+'vitesse': 500, //rapidité du mouvement (automatique ou manuel, même paramètre).
+'auto':0, //activer (1) en utilisant vitesse_auto, désactiver (0) ou personnaliser ("custom"). Voir la rubrique Timer.
+'type':"none", //définit le role du slider dans le cas d'une liaison. Options: visualiseur, menu, none.
+'cible':"none", //spécifie l'id du slider compagnon dans le cas d'une liaison (ex: #slider_deux).
+'isolement': 0, //si actif, le slider est isolé par un fond noir transparent lors de son utilisation.
+'pc_only':0, //si actif, le slider sera éffacé sur tous les dispositifs mobiles.
+'loop':0, //si actif, le slider répetera son contenu indéfiniment, créant un rail infini.
+'liquide':1, //si actif, l'élément prendra, de façon élastique, toute la zone du slider.
+'drag':0, //permet la manipulation du slider aux doigts ou en cliquer/glisser à la souris.
+'creer_afficheur': 0, //crée automatiquement une zone avec l'image zoomée au dessus du slider.
+'fading_mode': 1, //remplace la transition en "déplacement de rail" par un fondu.
+'fading_type': 1, //mode 1: fondu blanc. mode 2: fondu enchainé entre deux images.
+'buffering_nbr': 1, //nombre d'image préchargé autour de l'image active. Nécessite l'utilisation de data-src au lieu de src.
+'fullscreen': 1, //affiche ou masque le bouton fullscreen.
+'bullets': 1, //affiche ou masque les puces du slider.
+'bullets_limit': 20, //limite de puces au délà de laquelle celles ci se transforme en un menu pages (ex: 7/22).
+'bullets_limit_mobile': 8, //identique à bullets_limit mais ne s'applique qu'en cas de mobile
+'arrows': 1, //affiche ou masque les flèches du slider.
+});
+</blockquote>
 
 </div><!-- \#wrapper -->
 <script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
@@ -72,25 +113,26 @@ $("pre, blockquote").on("click","span",function(){
 <script>
 $(document).ready(function(){
 	$('#slider_classique').sliker({
-	'nbr_li':1,
-	'vitesse_auto':3000,
-	'vitesse': 500,
-	'auto':0,
-	'type':"none",
-	'cible':"none",
-	'isolement': 0,
-	'pc_only':0,
-	'loop':0,
-	'liquide':1,
-	'drag':0,
-	'creer_afficheur': 0,
-	'fading_mode': 1,
-	'fading_type': 2,
-	'buffering_nbr': 1,
-	'fullscreen': 1,
-	'bullets': 1,
-	'bullets_limit': 20,
-	'arrows': 1,
+	'nbr_li':1, //nombre d'éléments qui défilent à chaque mouvement.
+	'vitesse_auto':3000, //temps entre deux mouvements automatiques.
+	'vitesse': 500, //rapidité du mouvement (automatique ou manuel, même paramètre).
+	'auto':0, //activer (1) en utilisant vitesse_auto, désactiver (0) ou personnaliser ("custom"). Voir la rubrique Timer.
+	'type':"none", //définit le role du slider dans le cas d'une liaison. Options: visualiseur, menu, none.
+	'cible':"none", //spécifie l'id du slider compagnon dans le cas d'une liaison (ex: #slider_deux).
+	'isolement': 0, //si actif, le slider est isolé par un fond noir transparent lors de son utilisation.
+	'pc_only':0, //si actif, le slider sera éffacé sur tous les dispositifs mobiles.
+	'loop':0, //si actif, le slider répetera son contenu indéfiniment, créant un rail infini.
+	'liquide':1, //si actif, l'élément prendra, de façon élastique, toute la zone du slider.
+	'drag':0, //permet la manipulation du slider aux doigts ou en cliquer/glisser à la souris.
+	'creer_afficheur': 0, //crée automatiquement une zone avec l'image zoomée au dessus du slider.
+	'fading_mode': 1, //remplace la transition en "déplacement de rail" par un fondu.
+	'fading_type': 1, //mode 1: fondu blanc. mode 2: fondu enchainé entre deux images.
+	'buffering_nbr': 1, //nombre d'image préchargé autour de l'image active. Nécessite l'utilisation de data-src au lieu de src.
+	'fullscreen': 1, //affiche ou masque le bouton fullscreen.
+	'bullets': 1, //affiche ou masque les puces du slider.
+	'bullets_limit': 20, //limite de puces au délà de laquelle celles ci se transforme en un menu pages (ex: 7/22).
+	'bullets_limit_mobile': 8, //identique à bullets_limit mais ne s'applique qu'en cas de mobile
+	'arrows': 1, //affiche ou masque les flèches du slider.
 	});
 });//doc ready
 </script>
